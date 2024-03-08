f41359fa3b91bf7d6ca6dc49a6744b78327b6776
Justin Coyne <jcoyne@justincoyne.com>
2024-03-08 08:01:40 -0600

    Initial commit

---
+++ lib/stanford/geo.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "geo/version"
+
+module Stanford
+  module Geo
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/stanford/geo/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Stanford
+  module Geo
+    VERSION = "0.1.0"
+  end
+end
